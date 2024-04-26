8603c4ccf2f49adfd2a2e87ea77f77a169af97a1
Luiz Felipe F M Costa <luiz@thenets.org>
2024-04-25 22:49:39 -0300

    Add more packages

--- src/01-install-os-packages.sh
+++ src/01-install-os-packages.sh
@@ -8,6 +8,7 @@ dnf upgrade -y
 # Repos
 dnf install -y dnf-plugins-core
 dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 
 # Groups
 dnf groupinstall -y "Development Tools" "Development Libraries"
@@ -18,6 +19,6 @@ dnf install -y \
     iproute bind9-next-utils hostname \
     python3-pip golang rust cargo \
     ansible \
-    terraform \
+    terraform gh \
     curl wget \
     vim nano
--- src/02-install-other-packages.sh
+++ src/02-install-other-packages.sh
@@ -7,4 +7,28 @@ pip install \
     ansible-navigator \
     ansible-creator \
     ansible-lint
-#pip install ansible-dev-tools
+
+# -- yq
+if ! command -v yq &> /dev/null; then
+    echo "yq not found in PATH, installing it..."
+    curl -sfL -o /usr/bin/yq https://github.com/mikefarah/yq/releases/download/v4.43.1/yq_linux_amd64
+    chmod 755 /usr/bin/yq
+fi
+
+# -- AWS CLI
+if ! command -v aws &> /dev/null; then
+    echo "aws not found in PATH, installing it..."
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip -qq awscliv2.zip
+    ./aws/install -i /usr/local/aws-cli -b /usr/bin
+    rm -f ./awscliv2.zip
+    rm -rf ./aws/
+fi
+
+# -- ROSA CLI
+if ! command -v rosa &> /dev/null; then
+    echo "rosa not found in PATH, installing it..."
+    wget -q https://github.com/openshift/rosa/releases/download/v1.2.37/rosa-linux-amd64
+    chmod +x rosa-linux-amd64
+    mv rosa-linux-amd64 /usr/local/bin/rosa
+fi
